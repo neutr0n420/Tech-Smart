692cd376bda8c9b9e0c4f7dfd32c08a148cd0956
KaKashi1210 <desmondaditya@gmail.com>
2023-07-22 23:31:49 +0530

    fix : fixed selectValues in index.tsx

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
 import { useRouter } from "next/navigation";
-
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
@@ -15,15 +14,10 @@ const Notes: FC<NotesProps> = ({ }) => {
 
 				<div className="my-10">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ut congue augue, in malesuada eros. Nam scelerisque, nisi ut viverra porta, urna enim varius turpis, sit amet vehicula mauris mi sed sem. Cras ullamcorper, erat at pulvinar euismod, nisi neque porta enim, vel fermentum augue elit vitae risus. Pellentesque commodo nibh eget lacus bibendum malesuada. Morbi at turpis neque. Mauris posuere faucibus nisi. Fusce scelerisque sem id lacus hendrerit, eu suscipit dui mollis. Sed non augue nunc. Nullam nunc ligula, maximus sit amet auctor ac, eleifend sed lorem. Integer eget elit id dui sollicitudin auctor. <br />
-
 					Proin suscipit elementum diam, eu venenatis arcu condimentum id. Suspendisse eleifend dolor augue, sed maximus urna viverra a. Pellentesque bibendum in ante id maximus. Nam venenatis eros sit amet velit dictum, at fermentum ante iaculis. Ut consectetur odio vel velit tincidunt sagittis. Sed imperdiet risus ex, quis ullamcorper risus efficitur in. Vestibulum mauris felis, congue ac leo in, interdum pharetra neque. Nunc quis mauris mauris. Donec fermentum diam id commodo pulvinar. Mauris maximus gravida augue, id aliquam ipsum laoreet posuere. Sed a risus elementum, auctor odio nec, faucibus lorem. Mauris non turpis et sapien pharetra mattis in at nisi.<br />
-
 					Nulla semper ex quis nisl aliquet rutrum. Nullam a quam id nulla sagittis lacinia. Donec convallis neque et interdum dignissim. Etiam vel imperdiet neque. Ut tempus quis turpis et iaculis. Quisque ultricies sollicitudin tempus. Nullam posuere augue in ultricies sodales. Mauris vitae iaculis urna, sit amet ornare ligula. Integer vitae purus non dui ornare aliquam eget eget leo.<br />
-
 					Donec congue malesuada sapien, vitae finibus nibh molestie at. Curabitur quis varius orci, accumsan fringilla nibh. Vestibulum nec laoreet dolor. Sed eu nunc ut sapien eleifend feugiat sed et eros. Etiam ornare a erat in auctor. Etiam varius in arcu dignissim interdum. Proin tempor purus diam. Nam laoreet felis id augue finibus dictum.<br />
-
 					Nam ullamcorper vehicula diam et rhoncus. Sed sodales ac lacus et venenatis. Donec vel est turpis. Etiam congue venenatis massa, eu posuere nibh cursus vel. Nullam at euismod diam. Quisque sit amet neque vitae nunc pulvinar vulputate. Etiam tempor feugiat mauris in tincidunt.<br />
-
 				</div>
 
 				<Button
@@ -42,14 +36,13 @@ const Prompt = ({ }) => {
 
 	return (
 		<div className="flex flex-row gap-3 w-max self-end">
-			<Input
+			<Input 
 				readOnly
 				onClick={() => router.push('/')}
 				placeholder="Generate new notes"
 			/>
 			<Button
-				onClick={() => router.push('/')}
-			>
+				onClick={() => router.push('/')} >
 				Generate
 			</Button>
 		</div>
