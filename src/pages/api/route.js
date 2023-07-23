2a9ed2b23b25f6e8bdd39af8e97239bb8808c1af
aryan bramhane <aryanbramhane1@gmail.com>
2023-07-23 11:34:47 +0530

    refactor: deleted the folder of chat and the file of route

---
+++ src/pages/api/route.js
@@ -0,0 +1,13 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config)
+
+export default async function (req, res){
+  if(!config.apiKey){
+    res.status
+  }
+}
